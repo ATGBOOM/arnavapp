7e33677d4cae97db02b223dcc66526fb445c6bd2
ATGBOOM <atgboom2669@gmail.com>
2023-03-06 20:34:23 +0530

    Navigation Started

--- lib/home/home_screen.dart
+++ lib/home/home_screen.dart
@@ -2,48 +2,41 @@ import 'package:arnavapp/base/logger_utils.dart';
 import 'package:arnavapp/playground/playground_screen.dart';
 import 'package:arnavapp/splash/splash_screen.dart';
 import 'package:arnavapp/timer/timer_screen.dart';
+import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:salomon_bottom_bar/salomon_bottom_bar.dart';
+import '../routes/app_router.gr.dart';
 
 class HomeScreen extends StatelessWidget{
   String _TAG = "HomeScreen";
   final _logger = LoggerUtils();
   @override
   Widget build(BuildContext context) {
-   return Scaffold(
-     appBar: AppBar(
-       title: Text("Umang Vaish Bespoke"),
-     ),
-     body: Center(
-       child: Column(
-         mainAxisAlignment: MainAxisAlignment.center,
-         children: [
-           Text("hello"),
-           SizedBox(
-             height: 20,
+   return AutoTabsScaffold(
+     routes: [
+       FirstPageRouter(),
+       SecondPageRouter(),
+       ThirdPageRouter(),
+     ],
+     bottomNavigationBuilder: (BuildContext context,TabsRouter tabsrouter){
+       return SalomonBottomBar(
+         items: [
+           SalomonBottomBarItem(
+               icon: Icon(Icons.import_contacts_sharp),
+               title: Text("First Page")
            ),
-           ElevatedButton(
-               onPressed: (){
-                  Navigator.push(context, MaterialPageRoute(builder: (context ) => TimerScreen()));
-               },
-               child: Text("Start Timer")
+           SalomonBottomBarItem(
+               icon: Icon(Icons.import_contacts_sharp),
+               title: Text("Second Page")
            ),
-           ElevatedButton(
-               onPressed: (){
-                 Navigator.push(context, MaterialPageRoute(builder: (context ) => PlaygroundScreen()));
-               },
-               child: Text("smile")
-           ),
-           ElevatedButton(
-               onPressed: (){
-                 //Navigator.push(context, MaterialPageRoute(builder: (context ) => SplashScreen()))
-                 _logger.log(_TAG, "hello");
-               },
-               child: Text("smile")
-
+           SalomonBottomBarItem(
+               icon: Icon(Icons.import_contacts_sharp),
+               title: Text("Third Page")
            ),
          ],
-       ),
-     ),
+
+       );
+     },
    );
   }
 
--- lib/splash/splash_screen.dart
+++ lib/splash/splash_screen.dart
@@ -1,3 +1,7 @@
+import 'dart:async';
+
+import 'package:arnavapp/routes/app_router.gr.dart';
+import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
@@ -5,6 +9,13 @@ import 'package:flutter_hooks/flutter_hooks.dart';
 class SplashScreen extends HookWidget {
   @override
   Widget build(BuildContext context) {
+    useEffect(() {
+      Future.microtask(() {
+        Timer(Duration (seconds: 2),(){
+          context.router.navigate(HomeScreenRoute());
+        });
+      },);
+    }, const[]);
     return Scaffold(
       body: Center(
         child: Column(
